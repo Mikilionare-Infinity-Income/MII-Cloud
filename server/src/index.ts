94c95e9a1fd281f630b9aee340d8865c33da38d0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-06-06 11:39:31 +0100

    feat: add CORS support and update package dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,27 @@
-import express, { Router } from "express";
+import express, {Router} from "express";
 import morgan from "morgan";
-import { routes } from "./routes";
+import {routes} from "./routes";
+import {config} from "dotenv";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Dotenv
+config();
+
+// CORS
+app.use(
+  cors({
+    origin: process.env.FRONTEND || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // Morgan
 app.use(morgan("dev"));
